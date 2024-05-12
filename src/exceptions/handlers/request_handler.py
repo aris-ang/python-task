6f8438608acd99d6729f5824ef05aff04ea3a1a7
aris-ang <mrar0sgr@gmail.com>
2024-05-12 17:43:49 +0300

    Request Handler exceptions.

---
+++ src/exceptions/handlers/request_handler.py
@@ -0,0 +1,10 @@
+"""
+Exceptions raised by the Request Handler class
+"""
+
+
+class RequestError(Exception):
+    """
+    Exception raised on request-related method failures of the request handler.
+    """
+    pass
