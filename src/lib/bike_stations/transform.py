427f13721b83d15b63f01bf237255d3a009583f9
aris-ang <mrar0sgr@gmail.com>
2024-05-12 17:50:27 +0300

    Functions performing the requested transformations on the bike station lists.

---
+++ src/lib/bike_stations/transform.py
@@ -0,0 +1,48 @@
+"""
+Module containing functions useful for performing transformative actions
+on bike station lists
+"""
+
+
+def clean_up(bike_station_list):
+    """
+    Function that restructures each element of the initial retrieved bike
+    station list into the requested format of step one.
+    :param bike_station_list: The list to be restructured.
+    :type bike_station_list: list of dict
+    :return: The restructured list, as requested in the test specification.
+    :rtype: list of dict
+    """
+    return [
+        {
+            'id': x['id'],
+            'name': x['name'],
+            'active': x['status'] == 'aktiv',
+            'description': x['description'],
+            'boxes': x['boxes'],
+            'free_boxes': x['free_boxes'],
+            'free_bikes': x['free_bikes'],
+            'free_ratio': x['free_boxes']/x['boxes'],
+            'coordinates': [x['longitude'], x['latitude']]
+        } for x in bike_station_list if x['free_bikes'] > 0
+    ]
+
+
+async def merge_stations_with_addresses(bike_station_list, address_list):
+    """
+    Function that merges the bike station and corresponding address list
+    for step two. The result is a bike station list that includes addresses.
+    :param address_list: The address list
+    :type address_list: list of str
+    :param bike_station_list: The bike station list.
+    :type bike_station_list: list of dict
+    :return: The merged list
+    :rtype: list of dict
+    """
+    result = [
+        {
+            'address': total[1],
+            **total[0]
+        } for total in zip(bike_station_list, address_list)
+    ]
+    return result
